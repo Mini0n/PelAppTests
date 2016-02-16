da3abfa05483e1bc255db592560493381702bc41
mini0n <mini0n.000@gmail.com>
2016-02-16 14:13:34 -0600

    Added accordion to definitions
           modified:   client/stylesheets/styles.css
           modified:
           client/theory/templates/definitions/definition_item.html
           modified:
           client/theory/templates/definitions/definitions_list.html
           modified:   client/theory/templates/definitions_main.html
           modified:   client/theory/templates/terminos/termino_item.html
           modified:   client/theory/templates/terminos/terminos_list.html
           modified:   client/theory/templates/terminos_main.html
           modified:   server/definitionsDB.js

--- server/definitionsDB.js
+++ server/definitionsDB.js
@@ -1,644 +1,536 @@
 if (Definitions.find().count() === 0){
-
 Definitions.insert({
   name: 'Cirugía',
   text: 'Kheir - mano Ergon - trabajo\n \"Trabajo manual\" \nPráctica que implica la manipulación mecánica de las estructuras anatómicas con un fin médico, ya sea diagnóstico, terapéutico o pronóstico.',
-  url: '/'
+  url: '1'
 });
-
 Definitions.insert({ 
   name:'Lipoma',
   text: 'Tumor benigno costituido por la proliferación del tejido celular subcutáneo.',
-  url: '/'
+  url: '2'
 });
-
 Definitions.insert({ 
   name:'Desde la base de datos',
   text: 'Desde la base de datos :D.',
-  url: '/'
+  url: '3'
 });
-
 Definitions.insert({
   name: 'Abdomen agudo',
   text: 'Síndrome caracterizado por dolor abdominal agudo, progresivo y potencialmente mortal.',
-  url: '/'
+  url: '4'
 });
-
 Definitions.insert({
   name: 'Absceso',
   text: 'Acúmulo de pus en tejidos blandos.',
-  url: '/'
+  url: '5'
 });
-
 Definitions.insert({
   name: 'Anafilaxia',
   text: 'Reacción inmunitaria generalizada ante algún cuerpo extraño introducido al cuerpo humano y pone en peligro la vida del paciente.',
-  url: '/'
+  url: '6'
 });
-
 Definitions.insert({
   name: 'Anestesia general',
   text: 'Administración de fármacos que actúan a nivel del SNC para producir insensibilidad al dolor, pérdida de reflejos, relajación muscular y pérdida de la conciencia.',
-  url: '/'
+  url: '7'
 });
-
 Definitions.insert({
   name: 'Anestesia local',
   text: 'Medicamentos capaces de bloquear de manera reversible la conducción de una fibro o un tronco nervioso.',
-  url: '/'
+  url: '8'
 });
-
 Definitions.insert({
   name: 'Antisepsia',
   text: 'Empleo de sustancias químicas para inhibir el crecimiento, destruir o disminuir la cantidad de microogarnismo en TEJIDOS VIVOS.',
-  url: '/'
+  url: '9'
 });
-
 Definitions.insert({
   name: 'Aprendizaje basado en problemas (ABP).',
   text: 'Estrategia educativa que fomenta y permite el desarrollo del razonamiento y juicio clínico en el alumno.',
-  url: '/'
+  url: '10'
 });
-
 Definitions.insert({
   name: 'Asentimiento informado',
   text: 'Consentimiento informado en menores de edad. ',
-  url: '/'
+  url: '11'
 });
-
 Definitions.insert({
   name: 'Asepsia',
   text: 'Conjuto ode métodos aplicados para la conservación de la esterilidad.',
-  url: '/'
+  url: '12'
 });
-
 Definitions.insert({
   name: 'Bioética',
   text: 'Disciplina científica que estudia los aspectos éticos de la medicina y la biología en general.',
-  url: '/'
+  url: '13'
 });
-
 Definitions.insert({
   name: 'Bioseguridad',
   text: 'Tiene como propósito proteger la salud del equipo médico.',
-  url: '/'
+  url: '14'
 });
-
 Definitions.insert({
   name: 'Calibre',
   text: 'Diámetro del material de sutura. A mayor "ceros", menos gruesa es la sutura.',
-  url: '/'
+  url: '15'
 });
-
 Definitions.insert({
   name: 'Choque',
   text: 'Alteración hemodinámica caracterizada por hipoperfusión hística generalizada.',
-  url: '/'
+  url: '16'
 });
-
 Definitions.insert({
   name: 'Cierre primario',
   text: 'Afrontar los bordes de la herida utilizando sutura.',
-  url: '/'
+  url: '17'
 });
-
 Definitions.insert({
   name: 'Cierre secundario',
   text: 'No se afrontan bordes, dejando la herida cicatrizar. Imperativo usar en las heridas INFECTADAS.',
-  url: '/'
+  url: '18'
 });
-
 Definitions.insert({
   name: 'Cirugía',
   text: 'Del griego chier, mano y ergon, trabajo. Sanación que se lleva a cabo por medio de las manos e instrumentos para ese propósito.',
-  url: '/'
+  url: '19'
 });
-
 Definitions.insert({
   name: 'Cirugía electiva',
   text: 'Cirugía indicada cuando el paciente se encuentra estable y en caso de presentar comorbilidades, estas se encuentran controladas. Envío programado al especialista. v. gr. Trasplante Renal.',
-  url: '/'
+  url: '20'
 });
-
 Definitions.insert({
   name: 'Compentencias de la FacMed UNAM',
   text: '1. Pensamiento crítico, juicio clínico, toma de decisiones y manejo de información 2. Aprendizaje autoregulado y permanente 3. Comunicación efectiva 4. Conocimiento y aplicación de las ciencias biomédicas, sociomédicas y clínicas. 5. Habilidades clínicas de diagnóstico, pronóstico, tratamiento y rehabilitación. 6. Profesionalismo, aspetos éticos y responsabilidades legales. 7. Salud poblacional y sistema de salud: promoción de la salud y prevención de la enfermedad 8. Desarrollo y crecimiento personal',
-  url: '/'
+  url: '21'
 });
 
 Definitions.insert({
   name: 'Consentimiento informado',
   text: 'Procedimiento mediante el cual se garantiza que el sujeto ha expresado voluntariamente su intención de participar en alguna investigación, después de haber comprendido la información que se le ha dado, acerca de los objetivos del estudio, los beneficios, las molestias, los posibles riesgos y las alternativas, sus derechos y responsabilidades.',
-  url: '/'
+  url: '22'
 });
-
 Definitions.insert({
   name: 'Consulta médica',
   text: 'Atención brindada por un profesional de la salud.',
-  url: '/'
+  url: '23'
 });
-
 Definitions.insert({
   name: 'Corte',
   text: 'Sección metódica y controlada de los tejidos suprayacentes al órgano por abordar.',
-  url: '/'
+  url: '24'
 });
-
 Definitions.insert({
   name: 'Criterios de eliminación',
   text: 'Causa que obliga a un sujeto previamente incluido en el estudio a ser retirado de este.',
-  url: '/'
+  url: '25'
 });
-
 Definitions.insert({
   name: 'Criterios de exclusión',
   text: 'Características de los sujetos que obligacon a no incluirlos en el estudio.',
-  url: '/'
+  url: '26'
 });
 
 Definitions.insert({
   name: 'Criterios de inclusión',
   text: 'Características que deben reunir los sujetos para ingresar al estudio.',
-  url: '/'
+  url: '27'
 });
-
 Definitions.insert({
   name: 'Decisión',
   text: 'Elegir una resolución con determinación, firmeza y juicio.',
-  url: '/'
+  url: '28'
 });
-
 Definitions.insert({
   name: 'Criterios de exclusión',
   text: ' Características de los sujetos que obligacon a no incluirlos en el estudio.',
-  url: '/'
+  url: '29'
 });
-
 Definitions.insert({
   name: 'Decúbito prono',
   text: 'Imagen.',
-  url: '/'
+  url: '30'
 });
-
 Definitions.insert({
   name: 'Decúbito supino',
   text: 'Imagen',
-  url: '/'
+  url: '31'
 });
-
 Definitions.insert({
   name: 'Deontología',
   text: 'Pautas establecidas dentro de una comunidad para resolver los conflictos de intereses entre sus miembros.',
-  url: '/'
+  url: '32'
 });
-
 Definitions.insert({
   name: 'Derechos del médico',
   text: '1. Derecho a ejercer la profesión de forma libre 2. Derecho a laborar en isntalaciones apropiadas 3. Derecho a tener a disposición los recursos que se requieren  4.Derecho a abstenerse de garantizar resultados 5. Derecho a recibir trato respetuoso 6. Derecho a tener educación médica continua. 7. Derecho a realizar actividades de investigación 8. Derecho a promover sus intereses profesionales 9. Derecho a salvaguardar su prestigio profesional 10. Derecho a percibir una remuneración por sus servicios',
-  url: '/'
+  url: '33'
 });
-
 Definitions.insert({
   name: 'Derechos del paciente',
   text: '1. Recibir atención medica adecuada 2. Recibir trato digno y respetuoso 3. Recibir información suficiente 4. Decidir libremente sobre su atención 5. Otorgar o negar su consentimiento 6. Derecho a la intimidad y toda información manejada con estricta privacidad 7. Derecho a una segunda opinión 8. Derecho a recibir atención médica de urgencia. 9. Derecho a contar con un expdiente clínico 10. Derecho a ser atendido cuando se inconforme',
-  url: '/'
+  url: '34'
 });
-
 Definitions.insert({
   name: 'Desinfección',
   text: 'Eliminar microorganismos de materiales NO VIVOS.',
-  url: '/'
+  url: '35'
 });
-
 Definitions.insert({
   name: 'Disección',
   text: 'Liberar estructuras anatómicas del tejido conectivo que les rodea. Puede ser roma o cortante.',
-  url: '/'
+  url: '36'
 });
-
 Definitions.insert({
   name: 'Diseño de la investigación',
   text: 'Define las características que tendra el estudio para contestar la pregunta de investigación.',
-  url: '/'
+  url: '37'
 });
-
 Definitions.insert({
   name: 'Dolo',
   text: 'Hacer lo que se sabe que no se debe hacer.',
-  url: '/'
+  url: '38'
 });
-
 Definitions.insert({
   name: 'Estudio abierto',
   text: 'El investigador conoce las condiciones de aplicación y el resultado de las variables.',
-  url: '/'
+  url: '39'
 });
-
 Definitions.insert({
   name: 'Estudio ciego',
   text: 'El investigador desconoce las variables.',
-  url: '/'
+  url: '40'
 });
-
 Definitions.insert({
   name: 'Estudio comparativo',
   text: 'Existen uno o mas grupos control.',
-  url: '/'
+  url: '41'
 });
-
 Definitions.insert({
   name: 'Estudio cuasiexperimental',
   text: 'El investigador controla parcialmente la maniobra experimental. No controla variables extrañas.',
-  url: '/'
+  url: '42'
 });
-
 Definitions.insert({
   name: 'Estudio de casos y controles',
   text: 'Análisis de efecto a causa. Se identifica sujeto con la enfermedad y se busca antecedentes de una posible causa.',
-  url: '/'
+  url: '43'
 });
-
 Definitions.insert({
   name: 'Estudio de cohortes',
   text: 'Análisis de causa a efecto. Necesariamente es longitudinal.',
-  url: '/'
+  url: '44'
 });
-
-
 Definitions.insert({
   name: 'Estudio descriptivo',
   text: 'Se estudia un solo grupo.',
-  url: '/'
+  url: '45'
 });
-
 Definitions.insert({
   name: 'Estudio experimental',
   text: 'El investigador tiene control sobre la maniobra experimental.',
-  url: '/'
+  url: '46'
 });
-
 Definitions.insert({
   name: 'Estudio longitudinal',
   text: 'Se lleva a cabo un seguimiento del fenómeno estudiado. Deben hacerse dos o mas mediciones.',
-  url: '/'
+  url: '47'
 });
-
 Definitions.insert({
   name: 'Estudio observacional',
   text: 'El investigador aprecia el fenómeno sin modificar las variables.',
-  url: '/'
+  url: '48'
 });
-
 Definitions.insert({
   name: 'Estudio retrospectivo',
   text: 'La recolección de datos se hace a partir de datos previamente recolectados. v. gr. Recolecto la información de los expedientes clínicos.',
-  url: '/'
+  url: '49'
 });
-
 Definitions.insert({
   name: 'Estudio transversal',
   text: 'No hay seguimiento, se realiza una sola medición.',
-  url: '/'
+  url: '50'
 });
-
 Definitions.insert({
   name: 'Estudio prospectivo',
   text: 'La recolcción de datos se planea a futuro. v. gr. El experimento inicia hoy y recolecto los resultados la próxima semana.',
-  url: '/'
+  url: '51'
 });
-
 Definitions.insert({
   name: ' Ética',
   text: 'Origen y esencia de la moral.',
-  url: '/'
+  url: '52'
 });
-
 Definitions.insert({
   name: ' Evaluación formativa',
   text: 'Tiene por objeto dar a conocer los resultados al estudiante en un determiando momento. Permite mejorar periodicamente su rendimiento. }',
-  url: '/'
+  url: '53'
 });
-
 Definitions.insert({
   name: 'Evaluación sumativa',
   text: 'Permite determinar si una persona alcanzó los objetivos educativos propuestos.',
-  url: '/'
+  url: '54'
 });
-
 Definitions.insert({
   name: 'Exposición',
   text: 'Presentación al cirujano de los planos y estructuras anatómicas mediante diverso instrumental. }',
-  url: '/'
+  url: '55'
 });
-
 Definitions.insert({
   name: 'Fowler',
   text: 'Imagen',
-  url: '/'
+  url: '56'
 });
-
 Definitions.insert({
   name: ' Fuerza de tensión',
   text: 'Libras que soporta una sutura antes de romperse',
-  url: '/'
+  url: '57'
 });
-
 Definitions.insert({
   name: 'Ginecológica o litotomía',
   text: 'Imagen.',
-  url: '/'
+  url: '58'
 });
-
 Definitions.insert({
   name: 'Guía de práctica clínica',
   text: 'Instrumentos o caminos sistematizados que trasladan la evidencia científica al tratamiento del paciente.',
-  url: '/'
+  url: '59'
 });
-
 Definitions.insert({
   name: 'Hematoma subungueal',
   text: 'Colección de sangre entre el lecho y la lámina ungueal.',
-  url: '/'
+  url: '60'
 });
-
 Definitions.insert({
   name: 'Hemostasia',
   text: 'Procedimiento que se realiza para cohibir una hemorragia.  Puede ser temporal o definitica, manual o instrumental.',
-  url: '/'
+  url: '61'
 });
-
 Definitions.insert({
   name: 'Herida compleja',
   text: 'Forma irregular y bordes lesionados.',
-  url: '/'
+  url: '62'
 });
-
 Definitions.insert({
   name: ' Herida cutánea',
   text: 'Alteración de a integridad de la piel cauada por medios físicos, químicos o biológicos provocando una solución de continuidad en los tejidos blandos.',
-  url: '/'
+  url: '63'
 });
-
 Definitions.insert({
   name: 'Herida infectada',
   text: 'Herida con evolución de 12 horas o 24 horas en cara y manos. Incluye mordeduras. Signos claros de infección, supuración o tejido muerto.',
-  url: '/'
+  url: '64'
 });
-
 Definitions.insert({
   name: 'Herida limpia',
   text: 'Herida con menos de 6 horas de evolución, bordes nítidos y libres de bacterias.',
-  url: '/'
+  url: '65'
 });
-
 Definitions.insert({
   name: 'Herida simple',
   text: 'Bordes regulares y poco profundas.',
-  url: '/'
+  url: '66'
 });
-
 Definitions.insert({
   name: 'Hipertermia maligna',
   text: 'Síndrome muscular inducido químicamente por los anestésicos. Mortalidad mayor al 50% por lo cual es importante identificar pacientes en riesgo.',
-  url: '/'
+  url: '67'
 });
-
 Definitions.insert({
   name: 'Ignorancia',
   text: 'Hacer lo que no se sabe. }',
-  url: '/'
+  url: '68'
 });
-
 Definitions.insert({
   name: 'Impericia',
   text: 'Actuar con carencia de habilidades.',
-  url: '/'
+  url: '69'
 });
-
 Definitions.insert({
   name: 'Imprudencia',
   text: 'Actuar en forma temeraria o precipitada.',
-  url: '/'
+  url: '70'
 });
-
 Definitions.insert({
   name: 'Inobservancia',
   text: 'Actuar sin respetar las normas o reglamentos.',
-  url: '/'
+  url: '71'
 });
-
 Definitions.insert({
   name: 'Investigación',
   text: 'Actividad humana orientad a la creación de nuevo conocimiento. Proceso sistemático y organizado cuyo fin es responder a una pregunta. Utiliza el método científico.',
-  url: '/'
+  url: '72'
 });
-
 Definitions.insert({
   name: 'Investigación aplicada',
   text: 'Buscar soluciones a los problemas asistenciales.',
-  url: '/'
+  url: '73'
 });
-
 Definitions.insert({
   name: 'Investigación básica',
   text: 'Actuar sin respetar las normas o reglamentos.',
-  url: '/'
+  url: '74'
 });
-
 Definitions.insert({
   name: 'Investigación clínica',
   text: 'Utiliza experiencias obtenidas en humanos',
-  url: '/'
+  url: '75'
 });
-
 Definitions.insert({
   name: 'Investigación epidemiológica',
   text: 'Utilizando datos obtenidos de humanos.',
-  url: '/'
+  url: '76'
 });
-
 Definitions.insert({
   name: 'Investigación experimental',
   text: 'Utiliza laboratorios y material biológico.',
-  url: '/'
+  url: '77'
 });
-
 Definitions.insert({
   name: 'Investigación traslacional',
   text: 'Trasladar los resultados de la investigación básica a la clínica. "Del laboratorio a la cama de enfermo".',
-  url: '/'
+  url: '78'
 });
-
 Definitions.insert({
   name: 'Justo medio',
   text: 'Concepto creado por Aristóteles. Todas las nuestras acciones deben encontrarse entre el exceso y el defecto.',
-  url: '/'
+  url: '79'
 });
-
 Definitions.insert({
   name: 'Kraske o navaja sevillana',
   text: 'Imagen',
-  url: '/'
+  url: '80'
 });
-
 Definitions.insert({
   name: 'Lavado quirúrgico de manos por arrastre',
   text: 'Procedimiento cuyo principal objetivo es la eliminación de la mayor proporción de gérmenes y detritus celulares.',
-  url: '/'
+  url: '81'
 });
-
 Definitions.insert({
   name: 'Lipoma',
   text: 'Tumoración benigna constituida por células adiposas alojadas en el espacio subcutáneo.',
-  url: '/'
+  url: '82'
 });
-
 Definitions.insert({
   name: 'Mahometana o genupectoral',
   text: 'Imagen',
-  url: '/'
+  url: '83'
 });
-
 Definitions.insert({
   name: 'Medicina',
   text: 'Ciencia y arte de curar las enfermedades del cuerpo humano.',
-  url: '/'
+  url: '84'
 });
-
 Definitions.insert({
   name: 'Medicina basada en la evidencia (MBE)',
   text: 'Implementación de la práctica clínica basada en la mejor evidencia disponible y procedente de la investigación bibliográfica sistemática.',
-  url: '/'
+  url: '85'
 });
-
 Definitions.insert({
   name: 'Negligencia',
   text: 'Omisión o demora inexcusable de la atención médica.',
-  url: '/'
+  url: '86'
 });
-
 Definitions.insert({
   name: 'Nevo',
   text: 'Mácula pigmentada menor de  1 cm constituida por grupos celulares de células melanocíticas.',
-  url: '/'
+  url: '87'
 });
-
 Definitions.insert({
   name: 'Nosocomia',
   text: 'Hospitales para pobres.',
-  url: '/'
+  url: '88'
 });
-
 Definitions.insert({
   name: 'Onicocriptosis',
   text: 'Desproporción del lecho ungueal y el ancho de la placa. Patología común del primer ortejo.',
-  url: '/'
+  url: '89'
 });
-
 Definitions.insert({
   name: 'Panadizo',
   text: 'Infección del pulpejo del dedo.',
-  url: '/'
+  url: '90'
 });
-
 Definitions.insert({
   name: 'Paroniquia',
   text: 'Inflamación e infección de los bordes laterales de la uña.',
-  url: '/'
+  url: '91'
 });
-
 Definitions.insert({
   name: 'Quiste',
   text: 'Saco cerrado de epitelio derivado de anexos epidermicos y relleno de matercial semisólido derivado del epitelio.',
-  url: '/'
+  url: '92'
 });
-
-
 Definitions.insert({
   name: 'Razonamiento clínico',
   text: 'Establecer un diagnóstico con base en los datos obtenidos por interrogatorio, exploración física exámenes de laboratorio y gabinte.',
-  url: '/'
+  url: '93'
 });
-
 Definitions.insert({
   name: 'Sala de urgencias',
   text: 'Unidad operativa que admite, evalúa y califica al paciente no programado.',
-  url: '/'
+  url: '94'
 });
-
 Definitions.insert({
   name: 'Secreto profesional',
   text: 'Concepto esbozado por John Gregory en 1770',
-  url: '/'
+  url: '95'
 });
-
 Definitions.insert({
   name: 'Sims',
   text: 'Imagen',
-  url: '/'
+  url: '96'
 });
-
 Definitions.insert({
   name: 'Simulación',
   text: 'Proceso de apredizaje a través de modelos educativos que permiten semejar escenarios parecidos a la realidad.',
-  url: '/'
+  url: '97'
 });
-
 Definitions.insert({
   name: 'Sutura',
   text: 'Cualquier hilo de material utilizado para ligar vasos o afrontat tejidos.',
-  url: '/'
+  url: '98'
 });
-
 Definitions.insert({
   name: 'Sutura o síntesis',
   text: 'Aproximación de los tejidos con la finalidad de acelerar el proceso de cicatrización.',
-  url: '/'
+  url: '99'
 });
-
 Definitions.insert({
   name: 'Tiempos fundamentales de la cirugía',
   text: 'Corte Hemostasia Exposición Disección Sutura.',
-  url: '/'
+  url: '100'
 });
-
 Definitions.insert({
   name: 'Trendelemburg',
   text: 'Imagen',
-  url: '/'
+  url: '101'
 });
-
 Definitions.insert({
   name: 'Trendelemburg invertida',
   text: 'Imagen',
-  url: '/'
+  url: '102'
 });
-
 Definitions.insert({
   name: 'Triage',
   text: 'Término proveneinte del francés trier que consiste en clasificar a los pacientes en el servicio de urgencias jerarquizando prioridades. Utiliza código de colores: Rojo, amarillo y verde.',
-  url: '/'
+  url: '103'
 });
-
 Definitions.insert({
   name: 'Valetudinaria',
   text: 'Hospital de campaña.',
-  url: '/'
+  url: '104'
 });
-
 Definitions.insert({
   name: 'Variable dependiente',
   text: 'Resultado causado por efecto de la variable independiente. Es la que mide el investigador.',
-  url: '/'
+  url: '105'
 });
-
 Definitions.insert({
   name: 'Variable independiente',
   text: 'Variable manipulada por el investigador.',
-  url: '/'
+  url: '106'
 });
-
-
 }
 
 
